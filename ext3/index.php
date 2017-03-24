d97bf99af40dda9afb67da29b6bacadb6167dfd4
wodrow <wodrow451611cv@gmail.com>
2017-03-24 10:34:28 +0800

    init

--- ext3/index.php
+++ ext3/index.php
@@ -27,7 +27,53 @@
 </head>
 <body>
 <script>
-
+    /*new Ext.Viewport({
+     layout:"border",
+     items:[
+     {region:"north",title:"Up",height:50},
+     {region:"south",title:"Down",height:50},
+     {region:"west",title:"Left",width:200},
+     {region:"east",title:"Right",width:200},
+     {region:"center",title:"Center"}
+     ]
+     });*/
+    var pnNorth = new Ext.Panel({
+        id: 'pnNorth',
+        autoWidth: true,
+        heigth: 80,
+        frame: true,
+        region: 'north',
+        html: '这里放置页头内容'
+    });
+    var pnWest = new Ext.Panel({
+        id: 'pnWest',
+        title: '菜单项',
+        width: 200,
+        heigth: 'auto',
+        split: true,//显示分隔条
+        region: 'west',
+        collapsible: true
+    });
+    var pnCenter = new Ext.TabPanel({
+        region: 'center',
+        activeTab: 0,
+        items: [
+            {
+                title: '收件箱',
+                authHeight: true,
+                closable: true,//是否可关闭
+                html: '这里显示所收邮件。。。'
+            }
+        ]
+    });
+    var vp = new Ext.Viewport({
+        layout: "border",
+        items: [
+            pnNorth,
+            pnWest,
+            pnCenter
+        ]
+    });
 </script>
 </body>
 </html>
